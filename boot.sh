bb14d82546698e0924395d0e6dba5c6ea34fe0ea
isaacsquires <isaacsquires@me.com>
2020-05-14 17:19:48 +0100

    add timeout

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . .venv/bin/activate
 sleep 8
-exec gunicorn -b :5000 --access-logfile - --error-logfile -  --timeout 240 app:server
+exec gunicorn -b :5000 --access-logfile - --error-logfile -  --timeout 480 app:server
