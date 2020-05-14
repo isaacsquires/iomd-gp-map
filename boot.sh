4ccfa52d374126d289d3dbf82a31d1488a6b1326
isaacsquires <isaacsquires@me.com>
2020-05-14 18:36:57 +0100

    try just run straight from python

--- boot.sh
+++ boot.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-. .venv/bin/activate
-sleep 8
+# python -m venv .venv
+# . .venv/bin/activate
+# sleep 8
 # exec gunicorn --workers=5 -p 0.0.0.0:$PORT --timeout 240 app:server
 python app.py
