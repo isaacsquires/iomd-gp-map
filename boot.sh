d6f36dedd2289c99beaac2d764c2efd5fde6f289
isaacsquires <isaacsquires@me.com>
2020-05-14 18:29:48 +0100

    Try with procfile again

--- boot.sh
+++ boot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 . .venv/bin/activate
 sleep 8
-exec gunicorn --workers=5 -p 0.0.0.0:$PORT --timeout 240 app:server
+# exec gunicorn --workers=5 -p 0.0.0.0:$PORT --timeout 240 app:server
+python app.py
