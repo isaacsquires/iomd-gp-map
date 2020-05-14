faab8c0c3d3b050fc782b292e7ecdc6076962f8d
isaacsquires <isaacsquires@me.com>
2020-05-14 17:54:32 +0100

    include port  as env variable for boot

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 . .venv/bin/activate
 sleep 8
-exec gunicorn -b :5000 --access-logfile - --error-logfile -  --timeout 480 app:server
+exec gunicorn --workers=5 -p 0.0.0.0:$PORT --timeout 240 app:server
